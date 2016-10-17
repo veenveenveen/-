e93cb29171f16f42bb96c59a755a3d4165a0ffea
MAGICVEN <745685567@qq.com>
2016-10-17 08:57:31 +0800

    init

---
+++ 数据结构与算法/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  数据结构与算法
+//
+//  Created by 黄启明 on 2016/10/17.
+//  Copyright © 2016年 huatengIOT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
